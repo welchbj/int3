eb96930f32f4dc7a29c785a5cdd59777b27ae0ec
welchbj <12776611+welchbj@users.noreply.github.com>
2023-07-11 22:20:27 -0400

    Add stubbed tests for factoring

--- tests/test_factor.py
+++ tests/test_factor.py
@@ -1,3 +1,18 @@
 # TODO: Test various widths.
 
 # TODO: Passing an immediate that is too big.
+
+
+def test_invalid_immediate():
+    # TODO
+    assert False
+
+
+def test_width():
+    # TODO
+    assert False
+
+
+def test_allowed_and_forbidden_ops():
+    # TODO
+    assert False
