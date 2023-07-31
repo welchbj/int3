6d9786c0d3b87bff4f228823dc7ae0710ed05ca2
welchbj <12776611+welchbj@users.noreply.github.com>
2023-07-30 20:56:43 -0400

    Add dummy test file for semantic emitter

---
+++ tests/test_semantic_emitter.py
@@ -0,0 +1,5 @@
+# Tests for some corner casees in semantic emission. We usue x86_64 as the
+# architecture under test, but the intent here is for architecture-agnostic
+# tests that challenge the SemanticEmitter's logic.
+
+# TODO
