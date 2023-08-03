819a9f5d692a9c49a8988dc03322c4323693ee61
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-03 07:29:05 -0400

    Fix typo in test description

--- tests/test_semantic_emitter.py
+++ tests/test_semantic_emitter.py
@@ -1,4 +1,4 @@
-# Tests for some corner casees in semantic emission. We usue x86_64 as the
+# Tests for some corner cases in semantic emission. We usue x86_64 as the
 # architecture under test, but the intent here is for architecture-agnostic
 # tests that challenge the SemanticEmitter's logic.
 
