ba24cdf88a0a53e152d42200a79ada3b718a30e1
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-05 16:26:31 -0400

    Cleanup existing test files

--- tests/test_semantic_emitter.py
+++ tests/test_semantic_emitter.py
@@ -2,4 +2,20 @@
 # architecture under test, but the intent here is for architecture-agnostic
 # tests that challenge the SemanticEmitter's logic.
 
-# TODO
+# TODO: Check desired results.
+# TODO: Check no bad bytes present
+
+
+def test_short_circuit_xor():
+    # TODO
+    assert False
+
+
+def test_requires_factoring_force_sub():
+    # TODO
+    assert False
+
+
+def test_requires_factoring_force_xor():
+    # TODO
+    assert False
