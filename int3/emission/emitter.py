9024c4f97747d71fade833d49975add069ecb776
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-06 18:28:06 -0400

    Update choose() to support more than just Gadgets

--- int3/emission/emitter.py
+++ int3/emission/emitter.py
@@ -1,11 +1,15 @@
 import functools
+import random
 from dataclasses import dataclass, field
-from typing import Iterable
+from typing import Iterable, TypeVar
 
 from int3.context import Context
 from int3.errors import Int3SatError
 from int3.gadgets import Gadget
 from int3.strategy import Strategy
+
+
+T = TypeVar("T")
 
 
 @dataclass
@@ -14,25 +18,47 @@ class Emitter:
 
     ledger: str = field(init=False, default="")
 
-    def choose(self, gadget_iter: Iterable[Gadget]) -> Gadget:
-        """Choose a gadget based on bad byte constraints and the strategy."""
-        filtered_gadgets = []
+    def choose(self, item_iter: Iterable[T]) -> T:
+        """Choose an item from an iterable according to the active strategy..
 
-        for gadget in gadget_iter:
-            if not gadget.is_okay(self.ctx):
-                continue
+        For iterables of Gadgets, choices will be limited to those that meet
+        bad byte constraints.
+
+        """
+        is_gadget_seq = False
+        filtered_items = []
+
+        for item in item_iter:
+            if isinstance(item, Gadget):
+                is_gadget_seq = True
+
+                if not item.is_okay(self.ctx):
+                    continue
 
             if self.ctx.strategy == Strategy.GenerationSpeed:
                 # Short circuit for the first valid gadget.
-                return gadget
+                return item
             else:
-                filtered_gadgets.append(gadget)
+                filtered_items.append(item)
 
-        if not filtered_gadgets:
-            raise Int3SatError("Unable to identify any potential gadgets")
+        if not filtered_items:
+            if is_gadget_seq:
+                msg = "Unable to identify any satisfactory gadgets"
+            else:
+                msg = "Unable to identify a suitable choice"
 
-        assembled_len_wrapper = functools.partial(Gadget.assembled_len, ctx=self.ctx)
-        return min(filtered_gadgets, key=assembled_len_wrapper)
+            raise Int3SatError(msg)
+
+        if self.ctx.strategy == Strategy.Random:
+            return random.choice(filtered_items)
+        elif is_gadget_seq:
+            assembled_len_wrapper = functools.partial(
+                Gadget.assembled_len, ctx=self.ctx
+            )
+            return min(filtered_items, key=assembled_len_wrapper)
+        else:
+            # Default to just picking the first one.
+            return filtered_items[0]
 
     def emit(self, gadget: Gadget):
         """Record this gadget on this Emitter's ledger of assembly."""
