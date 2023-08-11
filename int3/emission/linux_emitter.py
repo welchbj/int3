0c99454d72c2f62ebdfb70f911b76d4bf1f6c86f
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-11 19:33:51 -0400

    Reorganize how Linux emitters define syscall metadata

--- int3/emission/linux_emitter.py
+++ int3/emission/linux_emitter.py
@@ -1,11 +1,23 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from contextlib import contextmanager, nullcontext
+from typing import Type
 
+from int3.architectures import Architecture, Architectures
 from int3.errors import Int3SatError
+from int3.gadgets import Gadget
 from int3.immediates import BytesImmediate, Immediate, IntImmediate
-from int3.registers import Registers, x86_64Registers, x86Registers
+from int3.registers import (
+    GpRegisters,
+    MipsRegisters,
+    Registers,
+    x86_64Registers,
+    x86Registers,
+)
 from int3.syscalls import SyscallConvention
 
+from .mips_emitter import MipsEmitter
 from .semantic_emitter import SemanticEmitter
 from .x86_64emitter import x86_64Emitter
 from .x86_emitter import x86Emitter
@@ -18,6 +30,10 @@ class LinuxEmitter(SemanticEmitter[Registers], ABC):
     def make_syscall_convention(self) -> SyscallConvention[Registers]:
         ...
 
+    @abstractmethod
+    def syscall_gadget(self, imm: int = 0) -> Gadget:
+        ...
+
     def syscall(self, num: int, *args: Registers | Immediate):
         syscall_con = self.make_syscall_convention()
         num_args = len(args)
@@ -25,6 +41,8 @@ class LinuxEmitter(SemanticEmitter[Registers], ABC):
         @contextmanager
         def _maybe_mov_or_push_syscall_arg(arg_idx: int):
             syscall_reg = syscall_con.reg_args[arg_idx]
+
+            # TODO: Maybe need to dynamically find a register for pushing (e.g., mips).
 
             arg_is_used = arg_idx < num_args
             wrapped_cm = (
@@ -41,6 +59,8 @@ class LinuxEmitter(SemanticEmitter[Registers], ABC):
             with wrapped_cm:
                 yield
 
+        # TODO: Need support for potentially passing some arguments on the stack.
+
         with _maybe_mov_or_push_syscall_arg(0):
             with _maybe_mov_or_push_syscall_arg(1):
                 with _maybe_mov_or_push_syscall_arg(2):
@@ -48,7 +68,7 @@ class LinuxEmitter(SemanticEmitter[Registers], ABC):
                         with _maybe_mov_or_push_syscall_arg(4):
                             with _maybe_mov_or_push_syscall_arg(5):
                                 self.mov(syscall_con.reg_num, num)
-                                self.emit(self.literal_syscall())
+                                self.emit(self.syscall_gadget())
 
     def open(
         self, pathname: Registers | BytesImmediate, flags: Registers | IntImmediate
@@ -116,6 +136,18 @@ class LinuxEmitter(SemanticEmitter[Registers], ABC):
 
         return self.write(fd=fd, buf=buf, count=len(buf))
 
+    @staticmethod
+    def get_emitter_cls_for_arch(
+        arch: Architecture[Registers, GpRegisters]
+    ) -> Type[LinuxEmitter[Registers]]:
+        match arch:
+            case Architectures.x86.value:
+                return Linuxx86Emitter
+            case Architectures.x86_64.value:
+                return Linuxx86_64Emitter
+            case Architectures.Mips.value:
+                return LinuxMipsEmitter
+
 
 class Linuxx86Emitter(x86Emitter, LinuxEmitter[x86Registers]):
     def make_syscall_convention(self) -> SyscallConvention[x86Registers]:
@@ -132,6 +164,9 @@ class Linuxx86Emitter(x86Emitter, LinuxEmitter[x86Registers]):
             ),
         )
 
+    def syscall_gadget(self, imm: int = 0) -> Gadget:
+        return Gadget(f"syscall")
+
 
 class Linuxx86_64Emitter(x86_64Emitter, LinuxEmitter[x86_64Registers]):
     def make_syscall_convention(self) -> SyscallConvention[x86_64Registers]:
@@ -147,3 +182,23 @@ class Linuxx86_64Emitter(x86_64Emitter, LinuxEmitter[x86_64Registers]):
                 "r9",
             ),
         )
+
+    def syscall_gadget(self, imm: int = 0) -> Gadget:
+        return Gadget(f"syscall")
+
+
+class LinuxMipsEmitter(MipsEmitter, LinuxEmitter[MipsRegisters]):
+    def make_syscall_convention(self) -> SyscallConvention[MipsRegisters]:
+        return SyscallConvention[MipsRegisters](
+            reg_result="$v0",
+            reg_num="$v0",
+            reg_args=(
+                "$a0",
+                "$a1",
+                "$a2",
+                "$a3",
+            ),
+        )
+
+    def syscall_gadget(self, imm: int = 0) -> Gadget:
+        return Gadget(f"syscall {hex(imm)}")
