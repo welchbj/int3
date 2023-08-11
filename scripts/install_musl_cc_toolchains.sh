9b92bdacd239e23022740d2fefd32296e807e89e
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-11 17:54:57 -0400

    Add completed musl.cc download/install script

--- scripts/install_musl_cc_toolchains.sh
+++ scripts/install_musl_cc_toolchains.sh
@@ -3,11 +3,19 @@
 set -uo pipefail
 
 download_url=https://musl.cc
-download_dir=/opt/musl.cc/downloads
+base_dir=/opt/musl.cc
+download_dir=$base_dir/downloads
+install_dir=$base_dir/install
+link_install_dir=/usr/local/bin
 
 if [[ ! -d $download_dir ]]; then
     sudo mkdir -p $download_dir
     sudo chown -R $(id -u):$(id -g) $download_dir
+fi
+
+if [[ ! -d $install_dir ]]; then
+    sudo mkdir -p $install_dir
+    sudo chown -R $(id -u):$(id -g) $install_dir
 fi
 
 pushd $download_dir >/dev/null
@@ -16,12 +24,36 @@ for url in $(curl -s $download_url | grep -- -cross); do
     tarball_name=$(echo $url | cut -d'/' -f4)
 
     if [[ -f $tarball_name ]]; then
-        echo "Cached: $tarball_name"
+        echo "Download cached: $tarball_name"
         continue
     fi
 
     wget --quiet $url
     echo "Downloaded: $tarball_name"
+done
+
+popd >/dev/null
+pushd $install_dir >/dev/null
+
+for tarball in $(ls $download_dir/*.tgz); do
+    triple=$(echo ${tarball##*/} | sed -e "s/-cross.tgz//")
+    triple_install_dir=$install_dir/$triple
+
+    if [[ -d $triple_install_dir ]]; then
+        echo "Install cached: $triple_install_dir"
+        continue
+    fi
+
+    mkdir -p $triple_install_dir
+    tar xzf $tarball -C $triple_install_dir
+
+    bin_dir=$triple_install_dir/$triple-cross/bin
+
+    for bin in $(ls $bin_dir); do
+        sudo  ln --force --symbolic $bin_dir/$bin $link_install_dir/$bin
+    done
+
+    echo "Installed: $triple_install_dir"
 done
 
 popd >/dev/null
