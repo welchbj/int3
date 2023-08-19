597d7db2f9c961930d2bb2a6afa3f9591972320f
welchbj <12776611+welchbj@users.noreply.github.com>
2023-08-19 11:31:29 -0400

    Shift LinuxEmitter test to use file I/O rather than stdout

--- tests/test_linux_emitter.py
+++ tests/test_linux_emitter.py
@@ -1,15 +1,29 @@
+import os
 import random
+import stat
 import string
+from pathlib import Path
 
-from int3 import Architecture, Context, LinuxEmitter, Platforms, assemble
+from int3 import (
+    Architecture,
+    Context,
+    GpRegisters,
+    LinuxEmitter,
+    Platforms,
+    Registers,
+    assemble,
+)
 
 from .qemu import parametrize_qemu_arch, run_in_qemu
 
 
 @parametrize_qemu_arch
-def test_linux_emitter_echo(arch: Architecture):
-    ctx = Context(architecture=arch, platform=Platforms.Linux.value)
-    emitter = LinuxEmitter.get_emitter_cls_for_arch(arch)(ctx)
+def test_linux_emitter_write_to_file(
+    arch: Architecture[Registers, GpRegisters], tmp_path: Path
+):
+    # We add "/" as a bad byte since we know this will be tmp_path generated
+    # paths.
+    ctx = Context(architecture=arch, platform=Platforms.Linux.value, bad_bytes=b"/")
 
     def _make_word(len_: int) -> bytes:
         alphabet = string.ascii_letters.encode()
@@ -20,10 +34,31 @@ def test_linux_emitter_echo(arch: Architecture):
     long_word = _make_word(arch.byte_size * 4)
 
     for word in [short_word, aligned_word, long_word]:
+        emitter = LinuxEmitter.get_emitter_cls_for_arch(arch)(ctx)
+
+        out_file = tmp_path / f"out_{word.decode()}"
+        assert not out_file.exists()
+
+        pathname = str(out_file).encode() + b"\x00"
+
         with emitter.stack_scope(ret=True):
-            emitter.echo(word)
+            # XXX
+            # emitter.breakpoint()
+
+            fd_reg = emitter.open(
+                pathname=pathname,
+                flags=os.O_RDWR | os.O_CREAT,
+                mode=stat.S_IRWXU,
+            )
+            emitter.echo(word, fd=fd_reg)
+
+        print("\nGenerated assembly:")
+        print(str(emitter))
+
         shellcode = assemble(ctx=ctx, assembly=str(emitter))
-        assert run_in_qemu(shellcode=shellcode, arch=arch).output == word
+        run_in_qemu(shellcode=shellcode, arch=arch)
+
+        assert out_file.read_bytes() == word + b"\x00"
 
 
 def test_linux_emitter_syscall_with_byte_arguments():
