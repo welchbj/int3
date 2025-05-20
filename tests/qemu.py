29df9465296ae4445cc1294eada5353cde379733
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-05-20 18:47:25 -0400

    Cleanup bad variable names in tests

--- tests/qemu.py
+++ tests/qemu.py
@@ -22,9 +22,9 @@ class QemuResult:
 
 
 QEMU_ARCHES = [
-    arch_meta.value
-    for arch_meta in Architectures
-    if arch_meta.value.qemu_name != "unsupported"
+    arch.value
+    for arch in Architectures
+    if arch.value.qemu_name != "unsupported"
 ]
 
 
@@ -36,9 +36,9 @@ parametrize_qemu_arch = pytest.mark.parametrize("arch", QEMU_ARCHES, ids=_name_g
 
 
 def compile_src(
-    arch_meta: Architecture, in_file: Path, out_file: Path, static: bool = True
+    arch: Architecture, in_file: Path, out_file: Path, static: bool = True
 ):
-    cc_bin = f"{arch_meta.toolchain_triple}-gcc"
+    cc_bin = f"{arch.toolchain_triple}-gcc"
     if (cc_path := shutil.which(cc_bin)) is None:
         pytest.fail(f"No available gcc binary {cc_bin}")
 
@@ -49,8 +49,8 @@ def compile_src(
     subprocess.check_output(args)
 
 
-def run_in_qemu(shellcode: bytes, arch_meta: Architecture, strace: bool = True):
-    qemu_bin = f"qemu-{arch_meta.qemu_name}"
+def run_in_qemu(shellcode: bytes, arch: Architecture, strace: bool = True):
+    qemu_bin = f"qemu-{arch.qemu_name}"
     if (qemu_path := shutil.which(qemu_bin)) is None:
         pytest.fail(f"No available qemu binary {qemu_bin}")
 
@@ -64,7 +64,7 @@ def run_in_qemu(shellcode: bytes, arch_meta: Architecture, strace: bool = True):
         shellcode_file.write(shellcode)
 
         compile_src(
-            arch_meta=arch_meta,
+            arch=arch,
             in_file=FilePaths.INT3_SHELLCODE_RUNNER_SRC,
             out_file=Path(runner_bin.name),
         )
