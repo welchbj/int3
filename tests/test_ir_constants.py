51fa33711599a9212df30af3bed5af55a0540d93
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-05-20 18:59:41 -0400

    Fix broken constant type unit tests

--- tests/test_ir_constants.py
+++ tests/test_ir_constants.py
@@ -1,23 +1,17 @@
-import pytest
-
-from int3 import Int3IrMismatchedTypeError, IrIntConstant, IrIntType
+from int3 import IrIntConstant
 
 
 def test_int_constant_factories_u8():
     for valid_value in [0, 0x10, 0xFF]:
-        const = IrIntConstant.u8(valid_value)
-        assert const.type_ == IrIntType.u8()
+        assert IrIntConstant.u8().is_representable(valid_value)
 
     for invalid_value in [-0xFF, -1, 0xFF + 1]:
-        with pytest.raises(Int3IrMismatchedTypeError):
-            IrIntConstant.u8(invalid_value)
+        assert not IrIntConstant.u8().is_representable(invalid_value)
 
 
 def test_int_constant_factories_i8():
     for valid_value in [-128, 0, 127]:
-        const = IrIntConstant.i8(valid_value)
-        assert const.type_ == IrIntType.i8()
+        assert IrIntConstant.i8().is_representable(valid_value)
 
     for invalid_value in [-129, 128]:
-        with pytest.raises(Int3IrMismatchedTypeError):
-            IrIntConstant.i8(invalid_value)
+        assert not IrIntConstant.i8().is_representable(invalid_value)
