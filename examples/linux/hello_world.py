82ca137593779a786a03a3eca14403a9a0a55eaf
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-04 23:16:27 -0400

    Simplify hello world example

--- examples/linux/hello_world.py
+++ examples/linux/hello_world.py
@@ -4,28 +4,15 @@ from int3 import Compiler
 
 cc = Compiler.from_str("linux/x86_64", bad_bytes=b"\x00")
 
-
-with cc.func.increment(int, cc.types.inat):
+with cc.func.increment(int, cc.types.i64):
     cc.ret(cc.args[0] + 1)
 
-
 with cc.func.main():
-    cc.breakpoint()
-
     var = cc.i(0xDEAD) + 0xBEEF
     result = cc.func.increment(var)
     cc.sys_exit(result)
 
-    # with cc.if_else(my_var < 0xCAFEBABE) as (if_, else_):
-    #     with if_:
-    #         cc.sys_exit(0)
-    #     with else_:
-    #         cc.sys_exit(1)
-
-
 sep = "=" * 80 + "\n"
 print(cc.llvm_ir(), file=sys.stderr)
 print(sep, file=sys.stderr)
-print(cc.to_asm(), file=sys.stderr)
-print(sep, file=sys.stderr)
-sys.stdout.buffer.write(cc.to_bytes())
+sys.stdout.buffer.write(cc.compile())
