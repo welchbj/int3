07617683f91f9eb8b2012d18f84cffca15f25792
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-05 20:32:41 -0400

    Remove unused iport

--- int3/codegen/code_generator.py
+++ int3/codegen/code_generator.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING
 
 from int3.architecture import Architecture, Architectures, RegisterDef
 from int3.assembly import assemble
