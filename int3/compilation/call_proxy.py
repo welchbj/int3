a659dd49dee9b4e00b4d62981b12b5ec95ba6704
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-05 21:09:49 -0400

    Add calling logic based on symtab resolution

--- int3/compilation/call_proxy.py
+++ int3/compilation/call_proxy.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, cast
 
+from llvmlite import ir as llvmir
+
 from int3.errors import Int3CompilationError
 
 from .types import IntType, IntVariable, PyIntValueType
@@ -17,16 +19,35 @@ class CallProxy:
     def __call__(self, *args: PyIntValueType) -> IntVariable | None:
         func = self.func
         compiler = func.compiler
+        symtab_ptr = compiler.current_func.args[0].wrapped_llvm_node
 
-        # Emit stub to resolve the pointer of the function we want to
-        # call.
-        # TODO
+        func_args = [symtab_ptr]
+        func_args.extend([arg.wrapped_llvm_node for arg in args])
+
+        # Emit stub to resolve the pointer of the function we want to call.
+        symtab_idx = compiler.i32(func.symtab_index).wrapped_llvm_node
+        func_ptr = compiler.builder.gep(
+            ptr=symtab_ptr,
+            indices=[symtab_idx],
+            source_etype=compiler.types.ptr.wrapped_type,
+        )
+
+        # This is a bad hack to trick llvmlite into generating LLVM IR that
+        # calls our function pointer rather than a defined function.
+        #
+        # XXX: Consider submitting a patch to llvmlite to change the call() API.
+        class _fake_func:
+            def get_reference(self) -> str:
+                return f'{func_ptr.name_prefix}"{func_ptr.name}"'
+
+            @property
+            def function_type(self) -> llvmir.FunctionType:
+                return func.llvm_func.function_type
 
         # Emit the call to the resolved function pointer.
-        # TODO
-        llvm_ret_value = compiler.current_func.llvm_builder.call(
-            fn=func.llvm_func,
-            args=[arg.wrapped_llvm_node for arg in args],
+        call_instr = compiler.current_func.llvm_builder.call(
+            fn=_fake_func(),
+            args=func_args,
         )
 
         if func.return_type == compiler.types.void:
@@ -37,7 +58,7 @@ class CallProxy:
             return_type = cast(IntType, func.return_type)
 
         return IntVariable(
-            compiler=compiler, type=return_type, wrapped_llvm_node=llvm_ret_value
+            compiler=compiler, type=return_type, wrapped_llvm_node=call_instr
         )
 
 
