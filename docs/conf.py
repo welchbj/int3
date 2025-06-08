0f7b18f53921b8d8dd304347fcd4df7cdab2635b
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-07 23:13:51 -0400

    Add initial Sphnix files and helper scripts for building docs site

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+# Configuration file for the Sphinx documentation builder.
+#
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "int3"
+author = "Brian Welch"
+year = datetime.now().year
+copyright = f"{year}, {author}"
+# TODO: Take version info from version.py
+release = "0.0.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
