6c7a158aab3f72c6addc2dc0342935edf3ab4e20
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-10 19:03:18 -0400

    Condense the Linux  hello world example

--- examples/linux/hello_world.py
+++ examples/linux/hello_world.py
@@ -4,12 +4,8 @@ from int3 import Compiler
 
 cc = Compiler.from_str("linux/x86_64")
 
-with cc.def_func.increment(int, int):
-    cc.ret(cc.args[0] + 1)
-
 with cc.def_func.main():
-    msg = cc.b(b"Hello, world")
-    num_written = cc.sys_write(fd=1, buf=msg)
+    num_written = cc.sys_write(fd=1, buf=b"Hello, world")
     cc.sys_exit(num_written)
 
 sys.stdout.buffer.write(cc.compile())
