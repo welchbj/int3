276bac6967b1d042953bae713b6274b7eef3f808
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-09 21:44:40 -0400

    Change hello world example to be built around the write syscall

--- examples/linux/hello_world.py
+++ examples/linux/hello_world.py
@@ -8,8 +8,8 @@ with cc.def_func.increment(int, int):
     cc.ret(cc.args[0] + 1)
 
 with cc.def_func.main():
-    var = cc.i64(0xDEAD) + 0xBEEF
-    result = cc.call.increment(var)
-    cc.sys_exit(result)
+    msg = cc.b(b"Hello, world")
+    num_written = cc.sys_write(fd=1, buf=msg)
+    cc.sys_exit(num_written)
 
 sys.stdout.buffer.write(cc.compile())
