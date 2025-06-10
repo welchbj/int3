170aff3c921b6ec454a699e51b13a80834c52fb4
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-10 19:11:37 -0400

    Add newline to hello world example

--- examples/linux/hello_world.py
+++ examples/linux/hello_world.py
@@ -5,7 +5,7 @@ from int3 import Compiler
 cc = Compiler.from_str("linux/x86_64")
 
 with cc.def_func.main():
-    num_written = cc.sys_write(fd=1, buf=b"Hello, world")
+    num_written = cc.sys_write(fd=1, buf=b"Hello, world\n")
     cc.sys_exit(num_written)
 
 sys.stdout.buffer.write(cc.compile())
