89cc8ec3cdb1e45f88c3a983e2a169ee06d4bc4b
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-13 18:15:47 -0400

    Add trap-based preservation of error status for linting and formatting scripts

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,5 +2,10 @@
 
 set -uxo pipefail
 
+# See: https://stackoverflow.com/a/73000327
+trap "RC=1" ERR
+
 ruff check --select I --fix .
 ruff format .
+
+exit $RC
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,12 @@
 
 set -uxo pipefail
 
+# See: https://stackoverflow.com/a/73000327
+trap "RC=1" ERR
+
 ruff check --select I .
 ruff format --check .
 mypy int3/ tests/
 pytest
+
+exit $RC
