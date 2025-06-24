32b58af0801a41ed937369c9b76ae32867966bd0
Brian Welch <12776611+welchbj@users.noreply.github.com>
2025-06-24 11:07:15 -0400

    Allow generation of Instruction from assembly text

--- int3/codegen/compiled_segment.py
+++ int3/codegen/compiled_segment.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 
 from int3.architecture import Architecture, RegisterDef
-from int3.assembly import disassemble
+from int3.assembly import assemble, disassemble
 from int3.platform import Triple
 
 from .instruction import Instruction
@@ -13,7 +15,7 @@ class CompiledSegment:
     raw_asm: bytes
     bad_bytes: bytes
 
-    all_instructions: tuple[Instruction, ...] = field(init=False)
+    instructions: tuple[Instruction, ...] = field(init=False)
     dirty_instructions: tuple[Instruction, ...] = field(init=False)
     tainted_regs: set[RegisterDef] = field(init=False)
     scratch_regs: set[RegisterDef] = field(init=False)
@@ -25,18 +27,36 @@ class CompiledSegment:
         )
         dirty_insns = tuple(insn for insn in all_insns if insn.is_dirty(self.bad_bytes))
 
-        object.__setattr__(self, "all_instructions", all_insns)
+        object.__setattr__(self, "instructions", all_insns)
         object.__setattr__(self, "dirty_instructions", dirty_insns)
         object.__setattr__(self, "tainted_regs", self._init_tainted_regs())
         object.__setattr__(self, "scratch_regs", self._init_scratch_regs())
+
+    @staticmethod
+    def from_asm(triple: Triple, asm: str, bad_bytes: bytes = b"") -> CompiledSegment:
+        assembled_asm = assemble(arch=triple.arch, assembly=asm)
+        return CompiledSegment(
+            triple=triple, raw_asm=assembled_asm, bad_bytes=bad_bytes
+        )
 
     @property
     def arch(self) -> Architecture:
         return self.triple.arch
 
+    @property
+    def is_clean(self) -> bool:
+        return len(self.dirty_instructions) == 0
+
+    @property
+    def raw(self) -> bytes:
+        return b"".join(insn.raw for insn in self.instructions)
+
+    def __bytes__(self) -> bytes:
+        return self.raw
+
     def _init_tainted_regs(self) -> set[RegisterDef]:
         tainted_regs = set()
-        for insn in self.all_instructions:
+        for insn in self.instructions:
             tainted_regs |= insn.tainted_regs
         return tainted_regs
 
@@ -49,14 +69,3 @@ class CompiledSegment:
 
     def scratch_reg_for_size(self, bit_size: int) -> RegisterDef:
         return next(reg for reg in self.scratch_regs if reg.bit_size == bit_size)
-
-    @property
-    def is_clean(self) -> bool:
-        return len(self.dirty_instructions) == 0
-
-    @property
-    def raw(self) -> bytes:
-        return b"".join(insn.raw for insn in self.all_instructions)
-
-    def __bytes__(self) -> bytes:
-        return self.raw
