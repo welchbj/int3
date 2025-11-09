e0ea6a35c9a8a2493e28ce2490423025db4ccd9d
welchbj <12776611+welchbj@users.noreply.github.com>
2025-11-09 15:34:23 -0500

    Define codegen Strategy enum

--- int3/codegen/__init__.py
+++ int3/codegen/__init__.py
@@ -1,3 +1,4 @@
 from .code_generator import CodeGenerator
 from .instruction import Instruction, MemoryOperand, OperandView
 from .segment import Segment
+from .strategy import Strategy
---
+++ int3/codegen/strategy.py
@@ -0,0 +1,6 @@
+from enum import Enum, auto
+
+
+class Strategy(Enum):
+    CompilationSpeed = auto()
+    CodeSize = auto()
