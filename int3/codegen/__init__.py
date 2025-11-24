506ab89cb85f66c6bdff1f0caac158b3c47400da
welchbj <12776611+welchbj@users.noreply.github.com>
2025-11-24 00:38:22 -0500

    Add RegisterListOperand to codegen module exports

--- int3/codegen/__init__.py
+++ int3/codegen/__init__.py
@@ -1,5 +1,5 @@
 from .choice import Choice, FluidSegment, Option
 from .code_generator import CodeGenerator
-from .instruction import Instruction, MemoryOperand, OperandView
+from .instruction import Instruction, MemoryOperand, OperandView, RegisterListOperand
 from .segment import Segment
 from .strategy import Strategy
